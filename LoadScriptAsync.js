3c43e74d0475b005bb4363a9ab2ebfd1c997ec6c
zhenghaofan <357034385@qq.com>
2015-12-03 19:49:53 +0800

    add

--- LoadScriptAsync.js
+++ LoadScriptAsync.js
@@ -30,3 +30,22 @@ function loadScript(urls,callback){
 }
 loadScript(urls);
 
+//正常情况下：
+
+var loadScript = function(url, callback) {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = url;
+  if (script.readyState) {
+    script.onreadystatechange = function() {
+      if (script.readyState == "loaded" || script.readyState == "complete") { 
+        script.onreadystatechange = null;
+        callback(); 
+      }
+    };
+  } else {
+    script.onload = function() { callback(); }; 
+  }
+  document.body.appendChild(script);
+};
+
